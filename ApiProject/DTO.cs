469351bb2d5f09f79560e10e447b88cbd2dcefe6
Dasha <Daria.Osipova.V@gmail.com>
2020-07-14 13:54:48 +0300

    Api Project Setup.

---
+++ ApiProject/DTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject
+{
+    public class DTO
+    {
+        public string Text { get; set; }
+        public string Number { get; set; }
+
+        public DTO(string text, string number)
+        {
+            Text = text;
+            Number = number;
+        }
+    }
+}
---
+++ ApiProject/JsonSerializer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ApiProject
+{
+    public class JsonSerializer
+    {
+        public string SerializeJson(string description, string number)
+        {
+            var sTO = new DTO(description, number);
+
+            var jsonObject = JsonConvert.SerializeObject(sTO);
+
+            return jsonObject;
+           
+        }
+    }
+}
