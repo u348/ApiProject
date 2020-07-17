dbccb6552a256b0700ee6af91ce998e49c67aea0
Dasha <Daria.Osipova.V@gmail.com>
2020-07-17 16:15:01 +0300

    Added another test project.

---
+++ Project.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Project.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Project.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67dc11e3-b3c6-4bfc-822c-fe303450b4df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using ApiProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var jsonSerializer = new JsonSerializer();
+
+            var jsonString = jsonSerializer.SerializeJson(
+                "Test description.",
+                "12345");
+        }
+    }
+}
